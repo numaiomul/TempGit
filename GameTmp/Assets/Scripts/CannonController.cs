148b3024c22bbbe28cfbc0cb6351999d794fe26d
Plapamaru <adrian.plapamaru@yahoo.com>
2015-11-14 12:05:31 +0200

    Cannon Added + Demigiant library

---
+++ GameTmp/Assets/Scripts/CannonController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class CannonController : MonoBehaviour {
+
+	[SerializeField] private int minEulerAngleZ;
+	[SerializeField] private int maxEulerAngleZ;
+	[SerializeField] private float timeRotate;
+
+	private void Awake () {
+		RotateToZMax ();
+	}
+
+	private void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			transform.DOKill ();
+		}
+	}
+
+	private void RotateToZMin () {
+		transform.DORotate (new Vector3 (transform.localEulerAngles.x, transform.localEulerAngles.y, minEulerAngleZ), 
+		                    timeRotate).OnComplete (RotateToZMax);
+	}
+
+	private void RotateToZMax () {
+		transform.DORotate (new Vector3 (transform.localEulerAngles.x, transform.localEulerAngles.y, maxEulerAngleZ), 
+		                    timeRotate).OnComplete (RotateToZMin);;
+	}
+ }
