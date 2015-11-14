90c740415a8f5990ad572d5b814481f3c8330572
numaiomul <rizea.alin@gmail.com>
2015-11-14 18:47:30 +0200

    Cannon Mega Update

--- GameTmp/Assets/Scripts/GameScene/CannonController.cs
+++ GameTmp/Assets/Scripts/GameScene/CannonController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class CannonController : MonoBehaviour {
 
---
+++ GameTmp/Assets/Scripts/GameScene/PowerBarUpdate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class PowerBarUpdate : MonoBehaviour {
+
+	public Transform powerBar;
+
+	public Image bar1,bar2;
+	
+	// Update is called once per frame
+	void Update () {
+		bar1.fillAmount = powerBar.localPosition.x;
+		bar2.fillAmount = powerBar.localPosition.x;
+	}
+}
