82c2aa715171d4e94cc16de205cded8b6805dd93
Plapamaru <adrian.plapamaru@yahoo.com>
2015-11-14 13:06:32 +0200

    Cannon update

--- GameTmp/Assets/Scripts/GameScene/CannonController.cs
+++ GameTmp/Assets/Scripts/GameScene/CannonController.cs
@@ -8,14 +8,45 @@ public class CannonController : MonoBehaviour {
 	[SerializeField] private int maxEulerAngleZ;
 	[SerializeField] private float timeRotate;
 
+	[SerializeField] private float minPowerBar;
+	[SerializeField] private float maxPowerBar;
+	[SerializeField] private float timePowerBar;
+	
+	[SerializeField] private float maxForce;
+
+	[SerializeField] private Rigidbody2D projectile;
+	[SerializeField] private Transform powerBar;
+
 	private void Awake () {
 		RotateToZMax ();
 	}
 
 	private void Update () {
 		if (Input.GetMouseButtonDown (0)) {
-			transform.DOKill ();
+			if (DOTween.IsTweening (transform)) {
+				transform.DOKill ();
+				MovePowerBarToMax ();
+				powerBar.gameObject.SetActive (true);
+			} else {
+				float force = (powerBar.transform.localPosition.x + Mathf.Abs (minPowerBar))/(maxPowerBar - minPowerBar);
+				LaunchTheProjectile (force * maxForce);
+			}
 		}
+	}
+
+	private void LaunchTheProjectile (float force) {
+		projectile.gameObject.SetActive (true);
+		projectile.AddForce (projectile.transform.right * force, ForceMode2D.Impulse);
+		enabled = false;
+		powerBar.DOKill ();
+	}
+
+	private void MovePowerBarToMin () {
+		powerBar.DOLocalMoveX (minPowerBar, timePowerBar).SetEase (Ease.InOutSine).OnComplete (MovePowerBarToMax);
+	}
+
+	private void MovePowerBarToMax () {
+		powerBar.DOLocalMoveX (maxPowerBar, timePowerBar).SetEase (Ease.InOutSine).OnComplete (MovePowerBarToMin);
 	}
 
 	private void RotateToZMin () {
