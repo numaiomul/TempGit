f21d8a64863180280d8852c8131d96e984dfb301
Plapamaru <adrian.plapamaru@yahoo.com>
2015-11-15 01:52:40 +0200

    Added particle emmiter to cannon

---
+++ GameTmp/Assets/Scripts/ScriptsOnLevels/Level1ParticleEmmiter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1ParticleEmmiter : MonoBehaviour {
+
+	public ParticleSystem particleForCamera;
+	public Vector3 startPosition;
+	public GameObject canon;
+
+	private void Awake () {
+		particleForCamera = Instantiate (particleForCamera.gameObject).GetComponent <ParticleSystem> ();
+		particleForCamera.transform.position = startPosition;
+		canon.SetActive(false);
+		Invoke ("SetOff", particleForCamera.duration/2);
+	}
+
+	private void SetOff () {
+		canon.SetActive(true);
+		enabled = false;
+	}
+}
