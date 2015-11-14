1681ee2bc013d50e31f93a4c29d2926f9e819304
numaiomul <rizea.alin@gmail.com>
2015-11-14 11:51:24 +0200

    Added endgame pop-up, projectile prefab

---
+++ GameTmp/Assets/Scripts/GameScene/EndScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScreen : MonoBehaviour {
+
+	public Button mainMenuBtn,resetBtn,nextBtn;
+
+	public void Show (bool _main, bool _reset, bool _next) {
+		gameObject.SetActive(true);
+		mainMenuBtn.gameObject.SetActive(_main);
+		resetBtn.gameObject.SetActive(_reset);
+		nextBtn.gameObject.SetActive(_next);
+	}
+	public void Hide() {
+		gameObject.SetActive(false);
+	}
+
+	public void OnMainMenu () {
+		Application.LoadLevel("MainMenu");
+	}
+	public void OnReset() {
+		Application.LoadLevel("GameScene");
+	}
+	public void OnNext() {
+		GlobalDefines.levelLoaded++;
+		Application.LoadLevel("GameScene");
+	}
+
+
+}
