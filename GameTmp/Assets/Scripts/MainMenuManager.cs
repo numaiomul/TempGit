fe34dce2149bc93c60d45772cf26e8c376aab25e
lithiil <tudorjamal@gmail.com>
2015-11-14 12:36:18 +0200

    Main Menu Scene And Buttons
    Buttons + main menu manager script

---
+++ GameTmp/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+    public GameObject PlayButton;
+    public GameObject OptionsButton;
+    public GameObject CreditsButton;
+	// Use this for initialization
+
+    public void OnPlayButton() {
+        GlobalDefines.levelLoaded = 1;
+        Application.LoadLevel("GameScene");
+
+    }
+    public void OnOptionsButton() {
+        this.gameObject.SetActive(false);
+    }
+    public void OnCreditsButton() {
+        this.gameObject.SetActive(false);
+    }
+}
