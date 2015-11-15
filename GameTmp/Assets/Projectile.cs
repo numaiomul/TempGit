d67e2c6558c7f9292a9259b23d917cf6bd366f48
numaiomul <rizea.alin@gmail.com>
2015-11-15 04:33:50 +0200

    stuff

---
+++ GameTmp/Assets/Projectile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public AudioSource audioRef;
+
+	public void OnCollisionEnter2D(Collision2D _col) {
+		audioRef.Play();
+	}
+}
