74f905f9d54bae1ff086acd06774a718a5fba9c4
Plapamaru <adrian.plapamaru@yahoo.com>
2015-11-15 03:53:49 +0200

    Some particle emmiter shits

--- GameTmp/Assets/Scripts/ScriptsOnLevels/Level1ParticleEmmiter.cs
+++ GameTmp/Assets/Scripts/ScriptsOnLevels/Level1ParticleEmmiter.cs
@@ -2,25 +2,11 @@
 using System.Collections;
 
 public class Level1ParticleEmmiter : MonoBehaviour {
-
-	public ParticleSystem particleForCamera;
-	public Vector3 startPosition;
-	public GameObject canon;
-
 	public ParticleSystem particleForPresent;
 	public Vector3 particleForePresentStartPosition;
 
 	private void Awake () {
-		particleForCamera = Instantiate (particleForCamera.gameObject).GetComponent <ParticleSystem> ();
-		particleForCamera.transform.position = startPosition;
 		particleForPresent = Instantiate (particleForPresent.gameObject).GetComponent <ParticleSystem> ();
 		particleForPresent.transform.position = particleForePresentStartPosition;
-		canon.SetActive(false);
-		Invoke ("SetOff", particleForCamera.duration/2);
-	}
-
-	private void SetOff () {
-		canon.SetActive(true);
-		enabled = false;
 	}
 }
