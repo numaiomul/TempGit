8af0d6162aeff23a56236767bd089f8995277254
Plapamaru <adrian.plapamaru@yahoo.com>
2015-11-15 08:23:29 +0200

    SnowFlake Update

---
+++ GameTmp/Assets/Scripts/RotateSnow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateSnow : MonoBehaviour {
+
+	private Vector3 rotateTo;
+
+	private void Awake () {
+		switch (Random.Range (0,3)) {
+		case 0:
+			rotateTo = Vector3.up;
+			break;
+		case 1:
+			rotateTo = Vector3.forward;
+			break;
+		case 2:
+			rotateTo = Vector3.back;
+			break;
+		}
+		//rotateTo *= Random.Range (0,5);
+	}
+
+	private void Update () {
+		transform.Rotate (rotateTo);
+	}
+}
